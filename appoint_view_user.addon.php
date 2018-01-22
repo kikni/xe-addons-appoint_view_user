d25bf0553ff73266b1fca03637d0af1db8aa1912
KIKNI <30348234+kikni@users.noreply.github.com>
2018-01-22 21:49:30 +0900

    Version 2.0
    버전 2.0 업데이트
    - 하위 XE 1.5 버전 이하 지원 중단
    - 1.9 버전 호환성 개선 및 버그 개선
    - lang 변수추가

--- appoint_view_user.addon.php
+++ appoint_view_user.addon.php
@@ -1,19 +1,29 @@
 <?php
-    if(!defined("__ZBXE__")) exit();
-
+    /* Copyright (C) KIKNI <http://www.kikni.com> */
+    if(!defined('__XE__')) exit();
     /**
      * @appoint_view_user.addon.php
-     * @author phiDel(phidel@foxb.kr) fork kikni(developer@kikni.com)
+     * @author 키큰아이(developer@kikni.com)
      * @brief 문서보기 지정 에드온
      **/
-
     //에러시 패스
     if($this->error) return;
+    
+    $lang->avu_nick_name = '닉네임';
+    $lang->avu_user_id = '아이디';
+    $lang->avu_default_group_1 = '준회원';
+    $lang->avu_default_group_2 = '정회원';
+    $lang->avu_admin_group = '관리그룹';
+    $lang->avu_all_group = '그룹 ';
+    $lang->avu_grant = '권한 설정';
+    $lang->avu_first_desc = '해당 "';
+    $lang->avu_middle_desc = '"에 한하여 보기 권한을 부여합니다.';
+    $lang->avu_last_desc = '(복수 등록은 , 로 구분)';
+    $lang->avu_list_desc = '님만 보실 수 있어요.';
 
     $addon_idx = 909090;
-    $addon_opt1 = $addon_info->is_nickname == 'S'?'소속 그룹':($addon_info->is_nickname == 'Y'?'닉네임':'아이디');
-    $addon_group_list = $addon_info->group_list?$addon_info->group_list:'준회원,정회원,관리그룹';
-
+    $addon_opt1 = $addon_info->is_nickname == 'S'? $lang->avu_all_group : ( $addon_info->is_nickname == 'Y' ? $lang->avu_nick_name : $lang->avu_user_id ); // 그룹, 이름, 닉네임 설정
+    $addon_group_list = $addon_info->group_list ? $addon_info->group_list : $lang->avu_default_group_1.','.$lang->avu_default_group_2.','.$lang->avu_admin_group; // 준회원, 정회원, 관리그룹 기본값 설정
 
     if($called_position == 'after_module_proc'){
         if($this->act=='dispBoardWrite'){
@@ -21,10 +31,10 @@
             $val = null;
             $val->module_srl = $this->module_srl;
             $val->idx = $addon_idx;
-            $val->name = '문서보기 권한';
+            $val->name = $lang->avu_grant; //권한
             $val->type = $addon_info->is_nickname == 'S'?'checkbox':'text';
             $val->default = $addon_info->is_nickname == 'S'?$addon_group_list:'';
-            $val->desc = '지정한 유저 ('.$addon_opt1.') 에게만 문서보기 권한을 줍니다.'.($addon_info->is_nickname == 'S'?'':' (복수 등록은 , 로 구분)');
+            $val->desc = $lang->avu_first_desc.$addon_opt1.$lang->avu_middle_desc.($addon_info->is_nickname == 'S'?'':$lang->avu_last_desc);
             $val->is_required = 'N';
             $val->search = 'N';
             $val->eid = 'addon_appoint_view_user';
@@ -67,9 +77,9 @@
 
                 if($val){
                     unset($args->title);
-                    $args->{preg_match('/^1.5/', __ZBXE_VERSION__) ? 'status' : 'is_secret'} = preg_match('/^1.5/', __ZBXE_VERSION__) ? 'SECRET' : 'Y';
+                    $args->is_secret = 'SECRET';
                     if($addon_info->is_display_user == 'Y'){
-                        $args->title = $addon_opt1.' "'.$val.'" 님만 보세요.';
+                        $args->title = $addon_opt1.$val.$lang->avu_list_desc;
                     }
                 }
 
@@ -79,25 +89,25 @@
             $document_srl = Context::get('document_srl');
 
             // 권한이 있으면 권한을 제거후 읽기만 가능하게
-            if($_SESSION['own_document'][$document_srl] && $_SESSION['appoint_view_user'][$document_srl]){
-                unset($_SESSION['own_document'][$document_srl]);
+            if($GLOBALS['own_document'][$document_srl] && $GLOBALS['appoint_view_user'][$document_srl]){
+                unset($GLOBALS['own_document'][$document_srl]);
                 $oDocument = Context::get('oDocument');
-                $oDocument->variables[preg_match('/^1.5/', __ZBXE_VERSION__) ? 'status' : 'is_secret'] = preg_match('/^1.5/', __ZBXE_VERSION__) ? 'PUBLIC' : 'N';
+                $oDocument->variables['status'] = 'PUBLIC';
                 Context::set('oDocument', $oDocument);
             }
 
-            unset($_SESSION['appoint_view_user'][$document_srl]);
+            unset($GLOBALS['appoint_view_user'][$document_srl]);
 
         }
 
     }elseif($called_position == 'before_module_proc'){
         if(($this->act=='dispBoardContent' || $this->act=='getBoardCommentPage') && Context::get('document_srl')){
 
-            unset($_SESSION['appoint_view_user'][$document_srl]);
+            unset($GLOBALS['appoint_view_user'][$document_srl]);
             $document_srl = Context::get('document_srl');
 
             $logged_info = Context::get('logged_info');
-            if(!$logged_info || $logged_info->is_admin == 'Y' || $logged_info->denied =='Y' || $_SESSION['own_document'][$document_srl]) return;
+            if(!$logged_info || $logged_info->is_admin == 'Y' || $logged_info->denied =='Y' || $GLOBALS['own_document'][$document_srl]) return;
 
             $args->document_srl = $document_srl;
             $tmp_output = executeQuery('addons.appoint_view_user.getDocumentExtra', $args);
@@ -109,17 +119,17 @@
                         $users = $logged_info->group_list;
                         $avuser = explode(',',$extra_vars->avuser);
                         foreach($avuser as $l_user){
-                            $_SESSION['appoint_view_user'][$document_srl] = $l_user && in_array($l_user, $users);
-                            if($_SESSION['appoint_view_user'][$document_srl]) break;
+                            $GLOBALS['appoint_view_user'][$document_srl] = $l_user && in_array($l_user, $users);
+                            if($GLOBALS['appoint_view_user'][$document_srl]) break;
                         }
                     }else{
                         $users = explode(',',$extra_vars->avuser);
                         $l_user = ($addon_info->is_nickname == 'Y'?$logged_info->nick_name:$logged_info->user_id);
-                        $_SESSION['appoint_view_user'][$document_srl] = $l_user && in_array($l_user,$users);
+                        $GLOBALS['appoint_view_user'][$document_srl] = $l_user && in_array($l_user,$users);
                     }
 
                     // 권한을 줘서 모든 정보를 얻어옴
-                    $_SESSION['own_document'][$document_srl] = $_SESSION['appoint_view_user'][$document_srl];
+                    $GLOBALS['own_document'][$document_srl] = $GLOBALS['appoint_view_user'][$document_srl];
                 }
             }
         }
